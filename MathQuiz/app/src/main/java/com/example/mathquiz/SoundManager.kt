f5ab38930f454428dfbc35f8e6c5bf9c54be5e37
Sayumi <156690349+SayumiNelithma@users.noreply.github.com>
2024-09-17 12:50:01 +0530

    First commit

---
+++ MathQuiz/app/src/main/java/com/example/mathquiz/SoundManager.kt
@@ -0,0 +1,31 @@
+package com.example.mathquiz
+
+import android.media.MediaPlayer
+
+class SoundManager(private val context: MainActivity) {
+
+    private var mediaPlayer: MediaPlayer? = null
+
+    fun playSound(resourceId: Int) {
+        // Release any existing MediaPlayer instance
+        mediaPlayer?.release()
+
+        // Create a new MediaPlayer instance for the specified sound resource
+        mediaPlayer = MediaPlayer.create(context, resourceId)
+
+        // Start playing the sound
+        mediaPlayer?.start()
+
+        // Release the MediaPlayer resources when the sound playback is complete
+        mediaPlayer?.setOnCompletionListener {
+            mediaPlayer?.release()
+            mediaPlayer = null
+        }
+    }
+
+    fun stopSound() {
+        mediaPlayer?.stop()
+        mediaPlayer?.release()
+        mediaPlayer = null
+    }
+}
